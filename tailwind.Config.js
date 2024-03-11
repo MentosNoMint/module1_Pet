de97ca30a38ff0188071418548ea2df5ea7c7eac
Daria <d.shkarban01@gmail.com>
2024-03-11 17:41:55 +0500

    first

---
+++ tailwind.Config.js
@@ -0,0 +1,12 @@
+
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        clifford: '#da373d',
+                        blackMain: '#1C1C1C',
+                        magnolia: '#F8F4FF'
+                    }
+                }
+            }
+        }
